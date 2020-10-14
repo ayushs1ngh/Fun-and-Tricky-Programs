0982d6d2c4520082e1edf7d9706fd8e58c8a92be
Tushar Gaurav <tushgaurav@yahoo.com>
2020-10-14 17:01:41 +0530

    added python programs

---
+++ Python/ClassInheritance/ClassInheritance.py
@@ -0,0 +1,17 @@
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+    def fare(self):
+        return self.capacity * 100
+
+class Bus(Vehicle):
+    def fare(self):
+        amount = super().fare()
+        amount += amount * 10 / 100
+        return amount
+
+School_bus = Bus("School Volvo", 12, 50)
+print("Total Bus fare is:", School_bus.fare())
