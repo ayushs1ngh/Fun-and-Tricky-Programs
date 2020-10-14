9a5311b089120da2abced39b155a20052de35e22
Ayush Singh <56454838+ayushs1ngh@users.noreply.github.com>
2020-10-14 16:10:23 +0530

    added program to print comments

---
+++ Java/To Print Comments/program_to_print_comments.java
@@ -0,0 +1,8 @@
+//To print comments as output or make comments execute
+public class Testing { 
+	public static void main(String[] args) 
+	{ 
+		// the line below this gives an output 
+		// \u000d System.out.println("comment executed"); 
+	} 
+} 
