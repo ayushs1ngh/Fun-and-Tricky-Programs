f51716b513d1485f7b2d55bceb980d8bdfeda8a0
bhild <bryanhildebrand1@gmail.com>
2020-10-26 08:56:49 -0700

    Update Reader.java

--- Java/Web Challenge/Reader.java
+++ Java/Web Challenge/Reader.java
@@ -2,16 +2,33 @@ package Reader;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
+import org.json.*;
+import com.google.gson.*;
 
 public class Reader {
 	public static void main(String[] args) throws MalformedURLException, IOException{
 		//get a url
-		String url = "https://docs.google.com/document/d/1ygnI0izMC6MGE5jK14e6mIzfu1wtyIq2N7rueqqlkrA/edit";
-		System.out.println(fromURL);
+		String url = "http://open.mapquestapi.com/geocoding/v1/reverse?key=SAd4Ez053FHMazmKomIl1l87zIrtLmFO&location=100,100&includeRoadMetadata=false&includeNearestIntersection=false\\\\r\\\\n";
+		System.out.println(readJsonFromUrl(url));
 	}
-	public static String fromURL(String url) throws MalformedURLException, IOException{
-		InputStream is = new URL(url).openStream();
-		return is.toString();
+	private static String readAll(Reader rd) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		while ((cp = rd.read()) != -1) {
+			sb.append((char) cp);
+		}
+		return sb.toString();//retruns the String of the JSON
+	}
+	
+	public static JSONObject  (String url) throws IOException, JSONException {
+		InputStream is = new URL(url).openStream();//gets the JSON
+		try {
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			String jsonText = readAll(rd);
+			JSONObject json = new JSONObject(jsonText);
+			return json;//makes JSON usable
+		} finally {
+			is.close();
+		}
 	}
 }
