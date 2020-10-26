13ed7534f072f5a09e2c3c00f302e14713a8fdfa
bhild <bryanhildebrand1@gmail.com>
2020-10-26 08:54:22 -0700

    added challenge a soultuon

--- Java/Web Challenge/Reader.java
+++ Java/Web Challenge/Reader.java
@@ -1,7 +1,17 @@
 package Reader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+
 public class Reader {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws MalformedURLException, IOException{
+		//get a url
+		String url = "https://docs.google.com/document/d/1ygnI0izMC6MGE5jK14e6mIzfu1wtyIq2N7rueqqlkrA/edit";
+		System.out.println(fromURL);
+	}
+	public static String fromURL(String url) throws MalformedURLException, IOException{
+		InputStream is = new URL(url).openStream();
+		return is.toString();
 	}
 }
