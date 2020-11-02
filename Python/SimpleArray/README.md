db328108e0e0026f4b276670048f4f44877de6c3
bhild <bryanhildebrand1@gmail.com>
2020-11-02 12:33:24 -0800

    added simpleArray

---
+++ Python/SimpleArray/README.md
@@ -0,0 +1,4 @@
+This is a short practice in python arrays.
+
+The goal is to print out any given array using a loop, inverting the array and printing again
+and using strings as arrays.
---
+++ Python/SimpleArray/SimpleArray.py
@@ -0,0 +1,18 @@
+def arrayPractice1(array):
+    for i in range(array):
+        print(array[i])
+
+
+def arrayPractice2(array):
+    array = array[::-1]
+    return array
+
+
+def arrayPractice3(array):
+    for i in range(array):
+        print(array[i])
+
+
+arrayPractice1([0, 1, 2, 3])
+print(arrayPractice1([0, 1, 2, 3]));
+arrayPractice3("hello")
